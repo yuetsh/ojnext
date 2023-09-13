ec3da43bda791022d3492addc927f9ed27a53dfe
yuetsh <517252939@qq.com>
2023-09-13 12:58:04 +0800

    temp.

--- src/oj/problem/components/TestCat2.vue
+++ src/oj/problem/components/TestCat2.vue
@@ -1,27 +1,28 @@
 <script lang="ts" setup>
-import { LANGUAGE } from "~/utils/types"
+// import { LANGUAGE } from "~/utils/types"
 
-interface Props {
-  lang: LANGUAGE
-  input: string
-}
-const props = withDefaults(defineProps<Props>(), {
-  lang: "C",
-  input: "",
-})
+// interface Props {
+//   lang: LANGUAGE
+//   input: string
+// }
+// const props = withDefaults(defineProps<Props>(), {
+//   lang: "C",
+//   input: "",
+// })
 function gotoTestCat() {
   const url = "https://code.hyyz.izhai.net/"
-  let id = 50
-  const lang = props.lang
-  if (lang === "Python3") id = 71
-  if (lang === "Java") id = 62
-  const payload = { input: props.input || "", id }
-  try {
-    const encoded = window.btoa(encodeURIComponent(JSON.stringify(payload)))
-    window.open(`${url}?stdin=${encoded}`, "_blank")
-  } catch (e) {
-    window.open(url, "_blank")
-  }
+  // let id = 50
+  // const lang = props.lang
+  // if (lang === "Python3") id = 71
+  // if (lang === "Java") id = 62
+  // const payload = { input: props.input || "", id }
+  // try {
+  //   const encoded = window.btoa(encodeURIComponent(JSON.stringify(payload)))
+  //   window.open(`${url}?stdin=${encoded}`, "_blank")
+  // } catch (e) {
+  //   window.open(url, "_blank")
+  // }
+  window.open(url, "_blank")
 }
 </script>
 <template>
