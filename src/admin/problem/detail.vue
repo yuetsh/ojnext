7ee8bef20c60c2ccfe863943b80ba8ed9e077495
yuetsh <517252939@qq.com>
2025-09-16 10:01:05 +0800

    update

--- src/admin/problem/detail.vue
+++ src/admin/problem/detail.vue
@@ -105,8 +105,8 @@ const difficultyOptions: SelectOption[] = [
 ]
 
 const languageOptions = [
-  { label: LANGUAGE_SHOW_VALUE["C"], value: "C" },
   { label: LANGUAGE_SHOW_VALUE["Python3"], value: "Python3" },
+  { label: LANGUAGE_SHOW_VALUE["C"], value: "C" },
   { label: LANGUAGE_SHOW_VALUE["C++"], value: "C++" },
 ]
 
