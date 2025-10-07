657047793dd327de6013f79cd10175653daf5083
yuetsh <517252939@qq.com>
2025-10-07 21:35:07 +0800

    fix

--- src/shared/composables/websocket.ts
+++ src/shared/composables/websocket.ts
@@ -62,7 +62,7 @@ export class BaseWebSocket<T extends WebSocketMessage = WebSocketMessage> {
   public status: Ref<ConnectionStatus> = ref<ConnectionStatus>("disconnected")
 
   constructor(config: WebSocketConfig) {
-    this.url = `${import.meta.env.PUBLIC_WS_UR}/${config.path}/`
+    this.url = `${import.meta.env.PUBLIC_WS_URL}/${config.path}/`
 
     this.maxReconnectAttempts = config.maxReconnectAttempts ?? 5
     this.reconnectDelay = config.reconnectDelay ?? 1000
