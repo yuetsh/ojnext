869a1424ddca1846d1c4b5bf19dd3c8d840d812d
yuetsh <517252939@qq.com>
2025-12-25 13:29:00 +0800

    update

--- src/oj/submission/list.vue
+++ src/oj/submission/list.vue
@@ -368,7 +368,7 @@ const flowchartColumns: DataTableColumn<FlowchartSubmissionListItem>[] = [
         <n-form-item
           v-if="userStore.isSuperAdmin && route.name === 'submissions'"
         >
-          <n-button text type="primary" @click="toggleStatisticPanel(true)">
+          <n-button quaternary type="primary" @click="toggleStatisticPanel(true)">
             数据统计
           </n-button>
         </n-form-item>
