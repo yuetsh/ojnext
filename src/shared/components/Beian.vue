335c3927e7dbd483c024140bc19a9361e092384d
yuetsh <517252939@qq.com>
2026-01-12 14:07:07 +0800

    update

--- src/shared/components/Beian.vue
+++ src/shared/components/Beian.vue
@@ -1,5 +1,9 @@
 <template>
   <n-space v-if="!hiddenICP" justify="center" class="beian">
+    <div>
+      &copy; 2026 判题狗 保留所有权利 采用
+      <n-button text @click="goCC">CC BY-NC 4.0</n-button> 许可协议
+    </div>
     <n-button text @click="goICP">浙ICP备2023044109号</n-button>
     <n-button text @click="goPublicSecurity">
       浙公网安备33100402331786号
@@ -16,6 +20,13 @@ function goICP() {
   window.open("https://beian.miit.gov.cn", "_blank")
 }
 
+function goCC() {
+  window.open(
+    "https://creativecommons.org/licenses/by-nc/4.0/deed.zh-hans",
+    "_blank",
+  )
+}
+
 function goPublicSecurity() {
   window.open(
     "https://beian.mps.gov.cn/#/query/webSearch?code=33100402331786",
@@ -26,12 +37,5 @@ function goPublicSecurity() {
 <style scoped>
 .beian {
   margin-bottom: 12px;
-}
-
-.beian img {
-  width: 14px;
-  height: 14px;
-  transform: translateY(2px);
-  margin-right: 4px;
 }
 </style>
